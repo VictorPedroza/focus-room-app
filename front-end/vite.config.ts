aac740db506551505f3c450e1d40629de983a4c9
VictorPedroza <victor.pedroza@protonmail.com>
2026-09-14 08:08:40 -0300

    📦 build:(front-end): Implement aliases to import using @

--- front-end/vite.config.ts
+++ front-end/vite.config.ts
@@ -2,6 +2,7 @@ import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 
 import tailwindcss from '@tailwindcss/vite'
+import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -9,4 +10,9 @@ export default defineConfig({
     react(),
     tailwindcss()    
   ],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
 })
