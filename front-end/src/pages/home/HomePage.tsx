88067a894c10fa0fb7b887af581524c5e79cda99
VictorPedroza <victor.pedroza@protonmail.com>
2026-09-14 08:48:32 -0300

    🐛 fix:(front-end): Remove unsed function on home page

--- front-end/src/pages/home/HomePage.tsx
+++ front-end/src/pages/home/HomePage.tsx
@@ -1,11 +1,4 @@
-import { useEffect } from "react"
-import { socket } from "../../shared/lib";
-
 export const HomePage = () => {
-    useEffect(() => {
-        socket.emit("join_room");
-    }, []); 
-
     return(
         <h1 className="text-white" >Home Page</h1>
     )
