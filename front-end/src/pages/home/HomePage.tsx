6c43df13749ec141d9d5ee2efdc66a3ec89bb652
VictorPedroza <victor.pedroza@protonmail.com>
2026-09-16 11:47:06 -0300

    ✨ feat:(front-end): Add pomodoro timer on home page

--- front-end/src/pages/home/HomePage.tsx
+++ front-end/src/pages/home/HomePage.tsx
@@ -1,5 +1,10 @@
+import { PomodoroTimer } from "@/shared/components"
+
 export const HomePage = () => {
     return(
-        <h1 className="text-white" >Home Page</h1>
+        <div className="flex flex-col items-center justify-center w-full h-full">
+            <h1 className="text-white pb-8 text-4xl font-bold">Timer Pomodoro</h1>
+            <PomodoroTimer />
+        </div>
     )
 }
