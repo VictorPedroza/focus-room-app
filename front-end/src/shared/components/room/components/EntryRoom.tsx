5225de0607d6735fd6cbcc16f51b14853b33ca25
VictorPedroza <victor.pedroza@protonmail.com>
2026-09-17 08:13:47 -0300

    ✨ feat:(front-end): Remove input component to add code and create a "input" with prefix as "FRSM-"

--- front-end/src/shared/components/room/components/EntryRoom.tsx
+++ front-end/src/shared/components/room/components/EntryRoom.tsx
@@ -39,12 +39,26 @@ export const EntryRoom = () => {
                 value={username}
                 onChange={(e) => setUsername(e.target.value)}
             />
-            <Input
-                label="Código:"
-                placeholder="FSRM-XXXX"
-                value={code}
-                onChange={(e) => setCode(e.target.value)}
-            />
+            <div className="mt-3 w-full flex flex-col">
+                <label htmlFor="code" className="pl-2 text-xs text-slate-600">Codigo:</label>
+                <div className="flex w-full px-3 items-center border border-gray-800 bg-slate-900 rounded-lg text-slate-600">
+                    <p>FRSM-</p>
+                    <input
+                        type="number"
+                        id="code"
+                        value={code}
+                        onChange={(e) => setCode(e.target.value)}
+                        className="h-10 bg-transparent
+                        text-sm
+                        text-slate-600
+                        outline-none
+                        placeholder:text-slate-600
+                        disabled:cursor-not-allowedx
+                        [appearance:textfield]
+                        [&::-webkit-inner-spin-button]:appearance-none
+                        [&::-webkit-outer-spin-button]:appearance-none"/>
+                </div>
+            </div>
             <p className="text-xs text-slate-600 text-center mt-1">Solicite o código para quem criou a sala</p>
             <Button
                 text="Entrar na sala →"
